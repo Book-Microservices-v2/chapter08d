26db2f030ee92a1988e1ed460bf6c579f27fb760
Moises Macero <mechero@users.noreply.github.com>
2020-08-03 12:24:33 +0200

    Base: Chapter 8c

---
+++ gamification/src/main/java/microservices/book/gamification/game/LeaderBoardController.java
@@ -0,0 +1,26 @@
+package microservices.book.gamification.game;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import microservices.book.gamification.game.domain.LeaderBoardRow;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * This class implements a REST API for the Gamification LeaderBoard service.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/leaders")
+@RequiredArgsConstructor
+class LeaderBoardController {
+
+    private final LeaderBoardService leaderBoardService;
+
+    @GetMapping
+    public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
+        return leaderBoardService.getCurrentLeaderBoard();
+    }
+}
